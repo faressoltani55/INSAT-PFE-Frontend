28cfc1df505d4e19e00f4a1cba5ddab09225e9ec
butterjack <jawher_bouhouch@yahoo.com>
2021-02-04 22:09:53 +0100

    add calendar

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+import timeGrigPlugin from '@fullcalendar/timegrid'; 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  timeGrigPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    FullCalendarModule,
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class CalendarModule { }
